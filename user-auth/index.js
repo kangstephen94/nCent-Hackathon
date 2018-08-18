190748564186a4ba0dde447a5925ab3e13c869da
Stephen Kang <kang.stephen94@gmail.com>
2018-08-18 16:11:31 -0700

    User auth

--- user-auth/index.js
+++ user-auth/index.js
@@ -1,12 +1,29 @@
 const passport = require("passport");
 const express = require("express");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require('./config.keys.js');
 
 const app = express();
 
 // Generic register, use specific google provider
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken) => {
+    //Google api services sends back an accessToken to retrieve user information
+    console.log(accessToken);
+  })
+);
 
+//Pass user off to passport, when directed to this route
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
 
 const PORT = process.env.PORT || PORT;
 app.listen(PORT);
