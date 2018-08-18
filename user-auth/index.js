61e5530107c09257789e3aee8eba6c80aafaad43
Stephen Kang <kang.stephen94@gmail.com>
2018-08-18 16:02:44 -0700

    Remove keys

--- user-auth/index.js
+++ user-auth/index.js
@@ -7,9 +7,6 @@ const app = express();
 // Generic register, use specific google provider
 passport.use(new GoogleStrategy());
 
-//613085555439-fvfmgtm0m8q64mkpjg1hlfqeckg80ngd.apps.googleusercontent.com clientID
-
-//ZyX6FoZ8f6rtJfrQ_q0HwLdC  clientSecret
 
 const PORT = process.env.PORT || PORT;
 app.listen(PORT);
