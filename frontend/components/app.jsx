e5a2b0736cb5f9ac7fe439db96e769d6d64f4ba6
therealmeyer <rmeyer@scu.edu>
2018-08-18 18:20:02 -0700

    Setup frontend correctly

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Route, Switch, Redirect } from "react-router-dom";
 // import { AuthRoute } from "../util/route_util";
 //   ProtectedRoute,
 //   UserProtectedRoute 
