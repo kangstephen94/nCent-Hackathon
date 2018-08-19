9b319b65acdfebfe45d3f7e1026b07a784c46c12
Stephen Kang <kang.stephen94@gmail.com>
2018-08-18 17:53:01 -0700

    Query into postgresdb with accessToken

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,18 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
+const { Client } = require('pg');
+
+
+const client = new Client ({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'ncnt-dev',
+  password: 'dickey',
+  port: 5432,
+})
+
+client.connect();
 
 // Generic register, use specific google provider
 passport.use(
@@ -11,10 +23,21 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
+      console.log(profile.id.toString());
       //Google api services sends back an accessToken to retrieve user information after redirected back to callback.
-      console.log(accessToken);
-      console.log(refreshToken);
-      console.log(profile);
+      client.query(`SELECT * FROM users WHERE googleId = ${profile.id.toString()}`)
+        .then( (res) => {
+          console.log(res)
+          if (res.rows.length !== 0) {
+            console.log('not empty')
+          } else {
+            // Need to also generate a walletID and insert it here
+            console.log('inserted')
+            console.log(profile.id)
+            client.query(`INSERT INTO users(googleId) VALUES(${profile.id.toString()})`);
+          }
+        })
+        .catch(e => console.error(e.stack))
     }
   )
 );
