6f5919c248477593cf347f811244d9195c80d03f
Stephen Kang <kang.stephen94@gmail.com>
2018-08-18 18:33:38 -0700

    Int conversion

--- services/passport.js
+++ services/passport.js
@@ -23,19 +23,18 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('' + profile.id);
+      console.log(profile.id);
       //Google api services sends back an accessToken to retrieve user information after redirected back to callback.
-      client.query(`SELECT * FROM users WHERE googleId = ${profile.id.toString()}`)
-      .then( (res) => {
-        console.log(res)
-        if (res.rows.length !== 0) {
-
-        } else {
-          // Need to also generate a walletID and insert it here
-          client.query(`INSERT INTO users(googleId) VALUES(${profile.id.toString()})`);
-        }
-      })
-      .catch(e => console.error(e.stack))
+      const email = profile.emails[0].value;
+      client.query(`SELECT * FROM users WHERE googleId = ${profile.id}`)
+        .then(res => {
+          if (res.rows.length !== 0) {
+          } else {
+            // Need to also generate a walletID and insert it here.
+            client.query(`INSERT INTO users(googleId) VALUES(${profile.id})`);
+          }
+        })
+        .catch(e => console.error(e.stack));b
     }
   )
 );
