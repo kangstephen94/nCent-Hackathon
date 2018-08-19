1c6b59d5e77de289eab31f740763ae16df499e22
therealmeyer <rmeyer@scu.edu>
2018-08-18 18:04:32 -0700

    frontend structure

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path");
+
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/event_cent.jsx",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [{
+      test: [/\.jsx?$/, /\.js?$/],
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      },
+    }]
+  },
+  target: 'node',
+  node: {
+    fs: 'empty'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
